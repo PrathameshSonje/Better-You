e7c0cb466b548875e6437f93fa21a96f438f8d86
PrathameshSonje <prathameshsonje147@gmail.com>
2023-11-17 00:36:45 +0530

    completed navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,12 @@ const Home = () => {
     <div className="w-full flex justify-center items-center min-h-screen grainy">
       <div className="w-full flex justify-around items-center px-10">
         <div className="flex-col items-start h-full">
-          <Typography variant="h2">Welcome to Better-You</Typography>
-          <Typography variant="h6" className="mt-4">A place where you can learn and grow</Typography>
-          <Typography variant="h6">Be <span className="font-bold">Better</span> than Yesterday</Typography>
-          <Button variant="contained" size="large" className="bg-blue-500 mt-5">Get started</Button>
-          <Button variant="contained" size="large" className="bg-blue-500 mt-5 mx-6">Explore courses</Button>
+          <Typography variant="h1">Welcome to,</Typography>
+          <Typography variant="h1" className="font-bold text-zinc-700">Better-You.</Typography> 
+          <Typography variant="h5" className="mt-4">A place where you can learn and grow and</Typography>
+          <Typography variant="h5">Be <span className="font-bold">Better</span> than Yesterday</Typography>
+          <Button variant="contained" size="large" className="bg-blue-500 mt-5">Join for Free</Button>
+          <Button variant= "outlined" size="large" className=" mt-5 ml-10">Explore courses</Button>
         </div>
         <div className="">
           <Image src="/board.jpg"
--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,33 +1,78 @@
 'use client'
 
-import { Button } from "@mui/material"
+import { Button, Typography } from "@mui/material"
 import Link from "next/link"
 import { useSession } from "next-auth/react"
+import Image from "next/image"
 
 const Appbar = () => {
 
     const { data: session } = useSession()
 
+    interface User {
+        name?: string | null | undefined;
+        email?: string | null | undefined;
+        image?: string | null | undefined;
+
+    }
+
+    const userEmail = session?.user?.email ? (
+        <Typography>
+            {session?.user?.email}
+        </Typography>
+    ) : null 
+
+    const userImage = session?.user?.image ? (
+        <Image 
+        src={session?.user?.image} 
+        alt="Profile pic"
+        width={40}
+        height={40}
+        className="border-1 border-gray-500 rounded-full p-1"
+        />
+    ) : null
+
     return (<nav className="fixed h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-            <Link 
-            href='/'
-            className="z-40 font-semibold ml-4">
+            <Link
+                href='/'
+                className="z-40 font-semibold ml-4 text-lg">
                 <span>
                     Better-You.
                 </span>
             </Link>
 
-            <div className="gotya items-center space-x-5 mr-4">
+            <div className="items-center flex space-x-5 mr-4">
+
+                <Link 
+                    href='/dashboard'
+                    className="text-zinc-600 items-center">
+                        Dashboard
+                </Link>
+
                 {session ? (<Link href='/api/auth/signout'>
-                    <Button variant="contained" className="bg-blue-500">
-                        Signout
+                    <Button variant="text">
+                        Sign out
                     </Button>
-                </Link>) : (<Link href='/api/auth/signin'>
+                </Link>) : (
+                    <Link href='/api/auth/signin'>
+                        <Button variant="text">
+                            Log in
+                        </Button>
+                    </Link>)}
+
+                {!session ? (<Link href='/api/auth/signin'>
                     <Button variant="contained" className="bg-blue-500">
-                        Signin
+                        Join for free
                     </Button>
-                </Link>)}
+                </Link>) : null}
+
+                {session ? (
+                    <div>
+                        {userImage}
+                    </div>
+                ) : null}
+
             </div>
         </div>
     </nav>)
